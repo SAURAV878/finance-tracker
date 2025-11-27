447a6b2d1423956db8f3b3f267467fb933430dad
Saurav <ghimiresaurav789@gmail.com>
2025-11-27 21:35:22 +0545

    feat: make a new model name Transaction

--- tracker/models.py
+++ tracker/models.py
@@ -20,3 +20,23 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.type})"
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date  = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class META:
+        ordering = ['-date', '-created_at'] #- means keep in descending, newset frist
+
+    def __str__(self):
+        return f"{self.type} - ${self.amount} - {self.date}" #define how trans apperas in admin panel.
