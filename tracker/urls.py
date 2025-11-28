5b8e75110135936701fb6388cecc2b5ff1047b14
Saurav <ghimiresaurav789@gmail.com>
2025-11-28 22:32:38 +0545

    feat: add delete transcations

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('',  views.dashboard, name = 'dashboard'),
     path('add/', views.add_transaction, name='add_transaction'),
     path('transaction/<int:pk>/edit/', views.edit_transaction, name='edit_transaction'),
+    path('transaction/<int:pk>/delete/', views.delete_transaction, name='delete_transaction'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -39,7 +39,7 @@ def add_transaction(request):
 
 @login_required
 def edit_transaction(request, pk):
-    transaction = get_object_or_404(Transaction, pk=pk, user=request.user) # Fetch the object
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
     if request.method == 'POST':
         form = TransactionForm(request.POST, instance=transaction, user=request.user)
         if form.is_valid():
@@ -49,6 +49,15 @@ def edit_transaction(request, pk):
         form = TransactionForm(instance=transaction, user=request.user)
     
     return render(request, 'tracker/transaction_form.html', {'form': form, 'title': 'Edit Transaction'})
+
+@login_required
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('tracker:dashboard')
+    
+    return render(request, 'tracker/transaction_confirm_delete.html', {'transaction': transaction})
 
 
 
