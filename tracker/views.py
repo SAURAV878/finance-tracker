cdf26823a85f7a107b34ae1c9cdf89115e3923de
Saurav <ghimiresaurav789@gmail.com>
2025-11-28 14:53:42 +0545

    chore: modify in views.py

--- tracker/views.py
+++ tracker/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from .models import Transaction
 
 # Create your views here.
 # views = handle requets and response of function
+@login_required
 
 def dashboard(request):#request = object conating info about HTTP request
     transactions = Transaction.objects.filter(user=request.user)
-    total_income = transactions.filter(type = 'income').aaggregate(Sum('amount')) ['amount__sum'] or 0
+    total_income = transactions.filter(type = 'income').aggregate(Sum('amount')) ['amount__sum'] or 0
     total_expense = transactions.filter(type = 'expense').aggregate(Sum('amount')) ['amount__sum'] or 0
 
     balance = total_income - total_expense
@@ -22,3 +24,5 @@ def dashboard(request):#request = object conating info about HTTP request
     
 
     return render(request, 'tracker/dashboard.html', context)
+
+
