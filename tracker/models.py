59bf2e975991864d845bb92e8dfa527af637be73
Saurav <ghimiresaurav789@gmail.com>
2025-12-02 13:18:26 +0545

    chore: modify in forms.py

--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     TRANSACTION_TYPE_CHOICES = (
         ('income', 'Income'),
@@ -11,6 +20,7 @@ class Transaction(models.Model):
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.date} - {self.description} - {self.get_type_display()} - {self.amount}"
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Transaction
+from .forms import TransactionForm
 
 def transaction_list(request):
     transactions = Transaction.objects.all()
     return render(request, 'tracker/transaction_list.html', {'transactions': transactions})
+
+def add_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm()
+    return render(request, 'tracker/add_transaction.html', {'form': form})
+
+def edit_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, 'tracker/edit_transaction.html', {'form': form})
